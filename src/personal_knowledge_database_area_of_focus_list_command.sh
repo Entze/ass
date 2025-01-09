87f189d10b028522aa845ccd8a7d95eed7192412
Lukas Grassauer <lukas@grassauer.eu>
2025-01-09 17:31:24 +0100

    Sort list when calling `ass pkd aof ls`

--- src/personal_knowledge_database_area_of_focus_list_command.sh
+++ src/personal_knowledge_database_area_of_focus_list_command.sh
@@ -6,4 +6,5 @@ if [[ $verbosity -ge 1 ]]; then
   gum log --structured --level "debug" -- "Parsing input" "PKD_DIR" "$PKD_DIR" "--pkd-dir" "${args[--pkd-dir]}" "pkd" "$pkd" "AREAS_SUBDIR" "$AREAS_SUBDIR" "--areas-subdir" "${args[--areas-subdir]}" "areas" "$areas" "--verbose" "${args[--verbose]}" "verbosity" "$verbosity"
 fi
 
-fdfind --search-path "${pkd}/${areas}" --exact-depth 1 --type directory --exec printf "%s\n" {/}
+# List all directories at level 1 of $pkd/$areas, only show the basename, then sort
+fdfind --search-path "${pkd}/${areas}" --exact-depth 1 --type directory --exec printf "%s\n" {/} | sort
