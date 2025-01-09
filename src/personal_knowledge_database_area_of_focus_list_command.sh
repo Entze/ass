504b12e035b45160f950e2925796c6675953c287
Lukas Grassauer <lukas@grassauer.eu>
2025-01-09 17:17:48 +0100

    Add first command including tests

---
+++ src/personal_knowledge_database_area_of_focus_list_command.sh
@@ -0,0 +1,9 @@
+pkd="${args[--pkd-dir]:-$PKD_DIR}"
+areas="${args[--areas-subdir]:-$AREAS_SUBDIR}"
+verbosity="${args[--verbose]:-0}"
+
+if [[ $verbosity -ge 1 ]]; then
+  gum log --structured --level "debug" -- "Parsing input" "PKD_DIR" "$PKD_DIR" "--pkd-dir" "${args[--pkd-dir]}" "pkd" "$pkd" "AREAS_SUBDIR" "$AREAS_SUBDIR" "--areas-subdir" "${args[--areas-subdir]}" "areas" "$areas" "--verbose" "${args[--verbose]}" "verbosity" "$verbosity"
+fi
+
+fdfind --search-path "${pkd}/${areas}" --exact-depth 1 --type directory --exec printf "%s\n" {/}
