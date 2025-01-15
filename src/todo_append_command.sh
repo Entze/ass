14282d0ab05c0a24237a92ad18390bf4dc3b7e14
Lukas Grassauer <lukas@grassauer.eu>
2025-01-15 14:54:57 +0100

    Implement todo append

---
+++ src/todo_append_command.sh
@@ -0,0 +1,18 @@
+todotxt_file="${args[--todotxt-file]:-$TODOTXT_FILE}"
+text="${args[text]}"
+verbosity="${args[--verbose]:-0}"
+
+if [ $verbosity -ge 1 ]; then
+  gum log --structured --level "debug" -- "todo append: Parsing input" "TODOTXT_FILE" "$TODOTXT_FILE" "--todotxt-file" "${args[--todotxt-file]}" "todotxt_file" "$todotxt_file" "TEXT" "${args[text]}" "text" "$text" "--verbose" "${args[--verbose]}" "verbosity" "$verbosity"
+fi
+
+if ! [ -w $todotxt_file ]; then
+ gum log --structured --level "error" -- "todo append: Cannot not write to" "todotxt_file" "$todotxt_file"
+ exit 1
+fi
+
+if [ -z "$text" ]; then
+  gum log --structured --level "info" -- "todo append: TEXT is empty"
+fi
+
+printf "%s\n" "$text" >> $todotxt_file
